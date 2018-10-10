a2475700f18289f63f70423818a3524ca365c066
_ttn <tranthaongc@gmail.com>
2018-10-10 20:26:13 +0700

    WOOSTIFY-20 #comment create task min JS file

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,71 +1,90 @@
+/**
+ * Gulp file
+ *
+ * @package woostify
+ */
+
 'use strict';
 
 let theme       = 'woostify',
-    site_name   = 'wootify',
-    gulp        = require( 'gulp' ),
-    babel       = require( 'gulp-babel' ),
-    autoLoad    = require( 'gulp-load-plugins' )(),
-    del         = require( 'del' ),
-    wpPot       = require( 'gulp-wp-pot' ),
-    browserSync = require( 'browser-sync' ),
-    runSequence = require( 'run-sequence' ),
-    sass        = require( 'gulp-sass' ),
-    sourcemaps  = require( 'gulp-sourcemaps' ),
-    globbing    = require( 'gulp-css-globbing' ),
-    concat      = require( 'gulp-concat' ),
-    uglify      = require( 'gulp-uglify' ),
-    buffer      = require( 'vinyl-buffer' );
+	site_name   = 'wootify',
+	gulp        = require( 'gulp' ),
+	babel       = require( 'gulp-babel' ),
+	autoLoad    = require( 'gulp-load-plugins' )(),
+	del         = require( 'del' ),
+	wpPot       = require( 'gulp-wp-pot' ),
+	browserSync = require( 'browser-sync' ),
+	runSequence = require( 'run-sequence' ),
+	sass        = require( 'gulp-sass' ),
+	sourcemaps  = require( 'gulp-sourcemaps' ),
+	globbing    = require( 'gulp-css-globbing' ),
+	concat      = require( 'gulp-concat' ),
+	uglify      = require( 'gulp-uglify' ),
+	rename      = require( 'gulp-rename' ),
+	vinylBuffer = require( 'vinyl-buffer' );
 
 
 /* SASS: `compressed` `expanded` `compact` `nested` */
 gulp.task( 'sass', () =>
-    gulp.src( 'style.scss' )
-        .pipe( globbing({
-            extensions: ['.scss']
-        }))
-        .pipe( sourcemaps.init() )
-        .pipe( sass( { outputStyle: 'expanded' } ).on( 'error', sass.logError ) )
-        .pipe( sourcemaps.write( '.' ) )
-        .pipe( gulp.dest( '.' ) )
+	gulp.src( 'style.scss' )
+	.pipe( globbing( {
+		extensions: [ '.scss' ]
+	} ) )
+	.pipe( sourcemaps.init() )
+	.pipe( sass( {
+		outputStyle: 'expanded'
+	} ).on( 'error', sass.logError ) )
+	.pipe( sourcemaps.write( '.' ) )
+	.pipe( gulp.dest( '.' ) )
 );
 
 /* CONSOLE */
-function handleError ( e ) {
-    console.log( e.toString() );
-    this.emit( 'end' );
+function handleError( e ) {
+	console.log( e.toString() );
+	this.emit( 'end' );
 }
 
 /* BROSWER SYNC */
 gulp.task( 'browser-sync', () =>
-    browserSync({
-        files: 'style.css',
-        proxy: "http://" + site_name + ".ss"
-    })
+	browserSync( {
+		files: 'style.css',
+		proxy: "http://" + site_name + ".ss"
+	} )
 );
 
 /* CREATE .POT FILE */
 gulp.task( 'pot', () => {
-    gulp.src( '**/*.php' )
-        .pipe( wpPot( {
-            domain: theme,
-            package: 'Haintheme'
-        } ) )
-        .on( 'error', handleError )
-        .pipe( gulp.dest( 'languages/' + theme + '.pot' ) );
+	gulp.src( '**/*.php' )
+		.pipe( wpPot( {
+			domain: theme,
+			package: 'Haintheme'
+		} ) )
+		.on( 'error', handleError )
+		.pipe( gulp.dest( 'languages/' + theme + '.pot' ) );
 } );
 
-/* CLEAN */
-gulp.task( 'clean', del.bind( null, ['build'] ) );
+
+/* MIN JS FILE */
+gulp.task( 'minJs', () => {
+	gulp.src( [ 'assets/js/**/*.js', '!assets/js/**/*.min.js'] )
+		.pipe( uglify() )
+		.pipe( rename( { suffix: '.min' } ) )
+		.pipe( gulp.dest( 'assets/js' ) );
+} );
 
 /* .POT */
-gulp.task( 'createPot', ['browser-sync'], () => {
-    gulp.watch( '**/*.php', ['pot'] );
-});
+gulp.task( 'createPot', [ 'browser-sync' ], () => {
+	gulp.watch( '**/*.php', [ 'pot' ] );
+} );
 
 /*WATCH*/
-gulp.task( 'watch', ['browser-sync'], () => {
-    gulp.watch( ['assets/css/sass/**/*.scss', 'style.scss'], ['sass'] );
-});
+gulp.task( 'watch', [ 'browser-sync' ], () => {
+	gulp.watch( [ 'assets/css/sass/**/*.scss', 'style.scss' ], [ 'sass' ] );
+	gulp.watch( [ 'assets/js/**/*.js', '!assets/js/**/*.min.js'], ['minJs'] );
+} );
 
 /* DEFAULT TASK */
-gulp.task( 'default', ['watch'] );
+gulp.task( 'default', [ 'watch', 'minJs' ] );
+
+/* CLEAN */
+gulp.task( 'clean', del.bind( null, [ 'build' ] ) );
