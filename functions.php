3c8114c3914516c096fc9feb3c2810baef216489
duongancol <mr.duongancol@gmail.com>
2019-03-08 13:44:30 +0700

    release version 1.1.1

--- functions.php
+++ functions.php
@@ -6,7 +6,7 @@
  */
 
 // Define constants.
-define( 'WOOSTIFY_VERSION', '1.1.0' );
+define( 'WOOSTIFY_VERSION', '1.1.1' );
 define( 'WOOSTIFY_THEME_DIR', get_template_directory() . '/' );
 define( 'WOOSTIFY_THEME_URI', get_template_directory_uri() . '/' );
 
