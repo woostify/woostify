89b7bf33efd7a00a168617c28397e412f941c84d
duongancol <mr.duongancol@gmail.com>
2019-08-02 11:47:45 +0700

    release version 1.3.2

--- Gulpfile.js
+++ Gulpfile.js
@@ -8,7 +8,7 @@
 
 let theme       = 'woostify',
 	site_name   = 'woostify',
-	theme_ver   = '1.3.1',
+	theme_ver   = '1.3.2',
 	gulp        = require( 'gulp' ),
 	zip         = require( 'gulp-zip' ),
 	babel       = require( 'gulp-babel' ),
