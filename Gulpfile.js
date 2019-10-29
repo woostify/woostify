40b734a21122ec5453e165a1935c2cfa7abe456e
duongancol <mr.duongancol@gmail.com>
2019-10-29 09:13:24 +0700

    release theme 1.3.8

--- Gulpfile.js
+++ Gulpfile.js
@@ -8,7 +8,7 @@
 
 let theme       = 'woostify',
 	site_name   = 'woostify',
-	theme_ver   = '1.3.7',
+	theme_ver   = '1.3.8',
 	gulp        = require( 'gulp' ),
 	zip         = require( 'gulp-zip' ),
 	babel       = require( 'gulp-babel' ),
