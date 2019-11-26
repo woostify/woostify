bf6bf801c6eeeac6ee94501f9fb5ad9fa541c227
ttn <tranthaongc@gmail.com>
2019-11-26 17:33:50 +0700

    issue sticky button on mobile

--- assets/js/woocommerce/woocommerce.js
+++ assets/js/woocommerce/woocommerce.js
@@ -42,7 +42,16 @@ document.addEventListener( 'DOMContentLoaded', function() {
 	shoppingBag();
 
 	window.addEventListener( 'scroll', function() {
-		if ( document.body.classList.contains( 'woocommerce-demo-store' ) && -1 === document.cookie.indexOf( 'store_notice' ) ) {
+		if (
+			( document.body.classList.contains( 'woocommerce-demo-store' ) && -1 === document.cookie.indexOf( 'store_notice' ) ) ||
+			(
+				(
+					document.body.classList.contains( 'has-order-sticky-button' ) ||
+					document.body.classList.contains( 'has-proceed-sticky-button' )
+				) &&
+				window.innerWidth < 768
+			)
+		) {
 			scrollingDetect();
 		}
 	} );
