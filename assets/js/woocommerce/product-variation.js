d1127f827a69fdfe4ca56d1eb4142931fe3d5af7
ttn <tranthaongc@gmail.com>
2020-03-24 15:10:01 +0700

    update sold-individually issue

--- assets/js/woocommerce/product-variation.js
+++ assets/js/woocommerce/product-variation.js
@@ -4,7 +4,7 @@
  * @package woostify
  */
 
- /* global woostify_woocommerce_variable_product_data */
+/* global woostify_woocommerce_variable_product_data */
 
 'use strict';
 
@@ -22,128 +22,147 @@ function productVariation( selector, form ) {
 		imageSrc       = image.prop( 'src' ),
 		imageSrcset    = image.prop( 'srcset' ) || '',
 		// Photoswipe + zoom.
-		photoSwipe     = imageWrapper.find( 'a' ),
-		photoSwipeSrc  = photoSwipe.prop( 'href' ),
+		photoSwipe    = imageWrapper.find( 'a' ),
+		photoSwipeSrc = photoSwipe.prop( 'href' ),
 		// Product thumbnail.
-		thumb          = gallery.find( '.thumbnail-item:eq(0)' ),
-		thumbSrc       = thumb.find( 'img' ).prop( 'src' );
+		thumb    = gallery.find( '.thumbnail-item:eq(0)' ),
+		thumbSrc = thumb.find( 'img' ).prop( 'src' );
 
 	if ( ! jQuery( variationsForm ).length ) {
 		return;
 	}
 
-	jQuery( document.body ).on( 'found_variation', variationsForm, function( event, variation ) {
-		// get image url form `variation`.
-		var fullSrc  = variation.image.full_src,
-			imgSrc   = variation.image.src,
-			thumbSrc = variation.image.thumb_src,
-			inStock  = variation.is_in_stock;
+	jQuery( document.body ).on(
+		'found_variation',
+		variationsForm,
+		function( event, variation ) {
+			// get image url form `variation`.
+			var fullSrc  = variation.image.full_src,
+				imgSrc   = variation.image.src,
+				thumbSrc = variation.image.thumb_src,
+				inStock  = variation.is_in_stock;
 
-		console.log( variation );
+			// Change src image.
+			image.removeAttr( 'srcset' );
+			thumb.find( 'img' ).prop( 'src', thumbSrc );
 
-		// Change src image.
-		image.removeAttr( 'srcset' );
-		thumb.find( 'img' ).prop( 'src', thumbSrc );
+			// Photoswipe + zoom.
+			photoSwipe.prop( 'href', fullSrc );
 
-		// Photoswipe + zoom.
-		photoSwipe.prop( 'href', fullSrc );
+			// Image loading.
+			imageWrapper.addClass( 'image-loading' );
+			image
+				.prop( 'src', fullSrc )
+				.one(
+					'load',
+					function() {
+						imageWrapper.removeClass( 'image-loading' );
+					}
+				);
 
-		// Image loading.
-		imageWrapper.addClass( 'image-loading' );
-		image
-			.prop( 'src', fullSrc )
-			.one( 'load', function() {
-				imageWrapper.removeClass( 'image-loading' );
-			} );
+			// Re-init zoom handle.
+			if ( 'function' === typeof( easyZoomHandle ) ) {
+				easyZoomHandle();
+			}
 
-		// Re-init zoom handle.
-		if ( 'function' === typeof( easyZoomHandle ) ) {
-			easyZoomHandle();
+			var jsSelector    = document.querySelector( selector ),
+				productImages = jsSelector ? jsSelector.querySelector( '.product-images' ) : false,
+				outStockLabel = productImages ? productImages.querySelector( '.woostify-out-of-stock-label' ) : false,
+				onSaleLabel   = productImages ? productImages.querySelector( '.woostify-tag-on-sale' ) : false;
+
+			// In stock.
+			if ( inStock ) {
+				// Update quantity for variation.
+				if ( variation.max_qty ) {
+					var inStock          = variation.max_qty <= 10 ? variation.max_qty : ( Math.floor( Math.random() * 66 ) + 10 ),
+						availability     = event.currentTarget.querySelector( '.woocommerce-variation-availability' ),
+						markupHtml       = '',
+						soldIndividually = event.currentTarget.closest( '.sold-individually' );
+
+					if ( ! soldIndividually ) {
+						markupHtml += '<div class="woostify-single-product-stock stock">';
+						if ( woostify_woocommerce_variable_product_data && woostify_woocommerce_variable_product_data.stock_label ) {
+							markupHtml += '<span class="woostify-single-product-stock-label">' + woostify_woocommerce_variable_product_data.stock_label.replace( '%s', variation.max_qty ) + '</span>';
+						}
+						markupHtml += '<div class="woostify-product-stock-progress">';
+						markupHtml += '<span class="woostify-single-product-stock-progress-bar" data-number="' + inStock + '"></span>';
+						markupHtml += '</div>';
+						markupHtml += '</div>';
+
+						if ( availability ) {
+							availability.innerHTML = markupHtml;
+						}
+					}
+
+					// Re-init stock progress bar.
+					if ( 'function' === typeof( woostifyStockQuantityProgressBar ) ) {
+						setTimeout(
+							function() {
+								woostifyStockQuantityProgressBar();
+							},
+							200
+						)
+					}
+				}
+
+				// Remove label out of stock.
+				if ( outStockLabel ) {
+					outStockLabel.remove();
+				}
+
+				// Update sale tag.
+				if ( onSaleLabel && woostify_woocommerce_variable_product_data.sale_tag_percent && variation.display_price != variation.display_regular_price ) {
+					onSaleLabel.innerHTML = '-' + Math.round( ( ( variation.display_regular_price - variation.display_price ) / variation.display_regular_price ) * 100 ) + '%';
+				}
+			} else if ( woostify_woocommerce_variable_product_data ) {
+				var outStockLabelHtml = '<span class="woostify-out-of-stock-label position-' + woostify_woocommerce_variable_product_data.out_of_stock_display + ' ' + woostify_woocommerce_variable_product_data.out_of_stock_square + '">' + woostify_woocommerce_variable_product_data.out_of_stock_text + '</span>';
+
+				if ( ! outStockLabel ) {
+					productImages.insertAdjacentHTML( 'beforeend', outStockLabelHtml );
+				}
+			}
 		}
-
-		var jsSelector    = document.querySelector( selector ),
-			productImages = jsSelector ? jsSelector.querySelector( '.product-images' ) : false,
-			outStockLabel = productImages ? productImages.querySelector( '.woostify-out-of-stock-label' ) : false,
-			onSaleLabel   = productImages ? productImages.querySelector( '.woostify-tag-on-sale' ) : false;
-
-		// In stock.
-		if ( inStock ) {
-			// Update quantity for variation.
-			if ( variation.max_qty ) {
-				var inStock      = variation.max_qty <= 10 ? variation.max_qty : ( Math.floor( Math.random() * 66 ) + 10 ),
-					availability = event.currentTarget.querySelector( '.woocommerce-variation-availability' ),
-					markupHtml   = '';
-
-				markupHtml += '<div class="woostify-single-product-stock stock">';
-				if ( woostify_woocommerce_variable_product_data && woostify_woocommerce_variable_product_data.stock_label ) {
-					markupHtml += '<span class="woostify-single-product-stock-label">' + woostify_woocommerce_variable_product_data.stock_label.replace( '%s', variation.max_qty ) + '</span>';
-				}
-				markupHtml += '<div class="woostify-product-stock-progress">';
-				markupHtml += '<span class="woostify-single-product-stock-progress-bar" data-number="' + inStock + '"></span>';
-				markupHtml += '</div>';
-				markupHtml += '</div>';
-
-				if ( availability ) {
-					availability.innerHTML = markupHtml;
-				}
-
-				// Re-init stock progress bar.
-				if ( 'function' === typeof( woostifyStockQuantityProgressBar ) ) {
-					setTimeout(
-						function() {
-							woostifyStockQuantityProgressBar();
-						},
-						200
-					)
-				}
-			}
-
-			// Remove label out of stock.
-			if ( outStockLabel ) {
-				outStockLabel.remove();
-			}
-
-			// Update sale tag.
-			if ( onSaleLabel && woostify_woocommerce_variable_product_data.sale_tag_percent && variation.display_price != variation.display_regular_price ) {
-				onSaleLabel.innerHTML = '-' + Math.round( ( ( variation.display_regular_price - variation.display_price ) / variation.display_regular_price ) * 100 ) + '%';
-			}
-		} else if ( woostify_woocommerce_variable_product_data ) {
-			var outStockLabelHtml = '<span class="woostify-out-of-stock-label position-' + woostify_woocommerce_variable_product_data.out_of_stock_display + ' ' + woostify_woocommerce_variable_product_data.out_of_stock_square + '">' + woostify_woocommerce_variable_product_data.out_of_stock_text + '</span>';
-
-			if ( ! outStockLabel ) {
-				productImages.insertAdjacentHTML( 'beforeend', outStockLabelHtml );
-			}
-		}
-	} );
+	);
 
 	// Reset variation.
-	jQuery( '.reset_variations' ).on( 'click', function( e ) {
-		e.preventDefault();
+	jQuery( '.reset_variations' ).on(
+		'click',
+		function( e ) {
+			e.preventDefault();
 
-		// Change src image.
-		image.prop( 'src', imageSrc );
-		image.attr( 'srcset', imageSrcset );
-		thumb.find( 'img' ).prop( 'src', thumbSrc );
+			// Change src image.
+			image.prop( 'src', imageSrc );
+			image.attr( 'srcset', imageSrcset );
+			thumb.find( 'img' ).prop( 'src', thumbSrc );
 
-		// Photoswipe + zoom.
-		photoSwipe.prop( 'href', photoSwipeSrc );
+			// Photoswipe + zoom.
+			photoSwipe.prop( 'href', photoSwipeSrc );
 
-		// Zoom handle.
-		if ( 'function' === typeof( easyZoomHandle ) ) {
-			easyZoomHandle();
+			// Zoom handle.
+			if ( 'function' === typeof( easyZoomHandle ) ) {
+				easyZoomHandle();
+			}
 		}
-	} );
+	);
 }
 
-document.addEventListener( 'DOMContentLoaded', function() {
-	productVariation( '.product-gallery' );
+document.addEventListener(
+	'DOMContentLoaded',
+	function() {
+		productVariation( '.product-gallery' );
 
-	// For Elementor Preview Mode.
-	if ( 'function' === typeof( onElementorLoaded ) ) {
-		onElementorLoaded( function() {
-			window.elementorFrontend.hooks.addAction( 'frontend/element_ready/global', function() {
-				productVariation( '.product-gallery' );
-			} );
-		} );
+		// For Elementor Preview Mode.
+		if ( 'function' === typeof( onElementorLoaded ) ) {
+			onElementorLoaded(
+				function() {
+					window.elementorFrontend.hooks.addAction(
+						'frontend/element_ready/global',
+						function() {
+							productVariation( '.product-gallery' );
+						}
+					);
+				}
+			);
+		}
 	}
-} );
+);
