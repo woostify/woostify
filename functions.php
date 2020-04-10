8d3c98513182d3f908453f816150e7ba4daf221d
ttn <tranthaongc@gmail.com>
2020-04-10 10:51:15 +0700

    divi builder issue

--- functions.php
+++ functions.php
@@ -41,7 +41,7 @@ if ( is_admin() ) {
 }
 
 // Compatibility.
-require_once WOOSTIFY_THEME_DIR . 'compatibility/class-woostify-divi-builder.php';
+require_once WOOSTIFY_THEME_DIR . 'inc/compatibility/class-woostify-divi-builder.php';
 
 /**
  * Note: Do not add any custom code here. Please use a custom plugin so that your customizations aren't lost during updates.
--- inc/compatibility/class-woostify-divi-builder.php
+++ inc/compatibility/class-woostify-divi-builder.php
@@ -37,6 +37,7 @@ if ( ! class_exists( 'Woostify_Divi_Builder' ) ) {
 		 */
 		public function __construct() {
 			add_action( 'init', array( $this, 'woostify_divi_builder_hooks' ) );
+			add_filter( 'woostify_customizer_css', array( $this, 'woostify_divi_builder_add_custom_css' ) );
 		}
 
 		/**
@@ -44,6 +45,16 @@ if ( ! class_exists( 'Woostify_Divi_Builder' ) ) {
 		 */
 		public function woostify_divi_builder_hooks() {
 			remove_action( 'woocommerce_before_shop_loop_item_title', 'et_divi_builder_template_loop_product_thumbnail', 10 );
+		}
+
+		/**
+		 * Custom css
+		 *
+		 * @param string $styles Stylesheet.
+		 */
+		public function woostify_divi_builder_add_custom_css( $styles ) {
+			$styles .= 'body.et-fb:not(.folded) #view { position: static; }';
+			return $styles;
 		}
 	}
 }
