6bad8a8cf35911473b123b688f10731895d3c840
duongancol <mr.duongancol@gmail.com>
2020-05-08 22:53:21 +0700

    release theme 1.5.8

--- Gulpfile.js
+++ Gulpfile.js
@@ -8,7 +8,7 @@
 
 let theme       = 'woostify',
 	site_name   = 'dev',
-	theme_ver   = '1.5.7',
+	theme_ver   = '1.5.8',
 	gulp        = require( 'gulp' ),
 	zip         = require( 'gulp-zip' ),
 	autoLoad    = require( 'gulp-load-plugins' )(),
