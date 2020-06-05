133c7b7f7622dd572fca75fe0ac21041d6f7a2d5
ttn <tranthaongc@gmail.com>
2020-06-05 15:15:37 +0700

    put header/footer back

--- single-elementor_library.php
+++ single-elementor_library.php
@@ -5,35 +5,19 @@
  * @package woostify
  */
 
-?>
-<!DOCTYPE html>
+get_header();
 
-<html <?php language_attributes(); ?>>
-	<head>
-		<meta charset="<?php bloginfo( 'charset' ); ?>">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="profile" href="http://gmpg.org/xfn/11">
-		<?php wp_head(); ?>
-	</head>
+// Do not display content on single Woo Builder post type.
+if ( ! woostify_is_elementor_editor() && is_singular( 'woo_builder' ) ) {
+	return get_footer();
+}
 
-	<body <?php body_class(); ?>>
-		<?php
-		wp_body_open();
+if ( have_posts() ) {
+	while ( have_posts() ) {
+		the_post();
 
-		// Do not display content on single Woo Builder post type.
-		if ( ! woostify_is_elementor_editor() && is_singular( 'woo_builder' ) ) {
-			return;
-		}
+		the_content();
+	}
+}
 
-		if ( have_posts() ) {
-			while ( have_posts() ) {
-				the_post();
-
-				the_content();
-			}
-		}
-
-		wp_footer();
-		?>
-	</body>
-</html>
+get_footer();
