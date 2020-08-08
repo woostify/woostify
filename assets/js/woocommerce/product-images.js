fd4e125b364169b67bd4563fcc194339c09f6cd1
ttn <thaobtf@outlook.com>
2020-08-08 17:28:37 +0700

    update gallery thumbnail

--- assets/js/woocommerce/product-images.js
+++ assets/js/woocommerce/product-images.js
@@ -8,7 +8,6 @@
 
 'use strict';
 
-
 // Carousel widget.
 function renderSlider( selector, options ) {
 	var element = document.querySelectorAll( selector );
@@ -27,20 +26,20 @@ function renderSlider( selector, options ) {
 
 // Create product images item.
 function createImages( fullSrc, src, size ) {
-	var item = '<figure class="image-item ez-zoom" itemprop="associatedMedia" itemscope itemtype="http://schema.org/ImageObject">';
-	item += '<a href=' + fullSrc + ' data-size=' + size + ' itemprop="contentUrl" data-elementor-open-lightbox="no">';
-	item += '<img src=' + src + ' itemprop="thumbnail">';
-	item += '</a>';
-	item += '</figure>';
+	var item  = '<figure class="image-item ez-zoom" itemprop="associatedMedia" itemscope itemtype="http://schema.org/ImageObject">';
+		item += '<a href=' + fullSrc + ' data-size=' + size + ' itemprop="contentUrl" data-elementor-open-lightbox="no">';
+		item += '<img src=' + src + ' itemprop="thumbnail">';
+		item += '</a>';
+		item += '</figure>';
 
 	return item;
 }
 
 // Create product thumbnails item.
 function createThumbnails( src ) {
-	var item = '<div class="thumbnail-item">';
-	item += '<img src="' + src + '">';
-	item += '</div>';
+	var item  = '<div class="thumbnail-item">';
+		item += '<img src="' + src + '">';
+		item += '</div>';
 
 	return item;
 }
@@ -52,12 +51,14 @@ function woostifyGalleryCarouselMobile() {
 		return;
 	}
 
-	var slider = tns({
-	    container: gallery.querySelector( '#product-images' ),
-	    items: 1,
-	    loop: false,
-		autoHeight: true
-	} );
+	var slider = tns(
+		{
+			container: gallery.querySelector( '#product-images' ),
+			items: 1,
+			loop: false,
+			autoHeight: true
+		}
+	);
 }
 
 // Sticky summary for list layout.
@@ -75,225 +76,266 @@ function woostifyStickySummary() {
 	var sidebar = new StickySidebar(
 		'.product-summary',
 		{
-	        innerWrapperSelector: '.summary.entry-summary',
-	        topSpacing: 50,
-	        bottomSpacing: 50,
-	        minWidth: 992
-    	}
-    );
+			innerWrapperSelector: '.summary.entry-summary',
+			topSpacing: 50,
+			bottomSpacing: 50,
+			minWidth: 992
+		}
+	);
 }
 
-document.addEventListener( 'DOMContentLoaded', function(){
-	var gallery           = document.querySelector( '.product-gallery' ),
-		productThumbnails = document.getElementById( 'product-thumbnail-images' );
+document.addEventListener(
+	'DOMContentLoaded',
+	function(){
+		var gallery           = document.querySelector( '.product-gallery' ),
+			productThumbnails = document.getElementById( 'product-thumbnail-images' );
 
-	// Product images.
-	var imageCarousel,
-		options = {
-			loop: false,
-			container: '#product-images',
-			navContainer: '#product-thumbnail-images',
-			items: 1,
-			navAsThumbnails: true,
-			autoHeight: true
-	}
-
-	// Product thumbnails.
-	var thumbCarousel,
-		thumbOptions = {
-			loop: false,
-			container: '#product-thumbnail-images',
-			gutter: 10,
-			items: 5,
-			nav: false,
-			controls: true
-	}
-
-	if (
-		window.matchMedia( '( min-width: 768px )' ).matches &&
-		gallery &&
-		gallery.classList.contains( 'vertical-style' )
-	) {
-		thumbOptions.axis = 'vertical';
-	}
-
-	if ( productThumbnails ) {
-		imageCarousel = tns( options );
-		thumbCarousel = tns( thumbOptions );
-	}
-
-	// Arrow event.
-	function arrowsEvent() {
-		if ( ! thumbCarousel ) {
-			return;
+		// Product images.
+		var imageCarousel,
+			options = {
+				loop: false,
+				container: '#product-images',
+				navContainer: '#product-thumbnail-images',
+				items: 1,
+				navAsThumbnails: true,
+				autoHeight: true
 		}
 
-		var buttons = document.querySelectorAll( '.product-images .tns-controls button' );
+		// Product thumbnails.
+		var firstImage       = gallery.querySelector( '.image-item img' ),
+			firstImageHeight = firstImage ? firstImage.offsetHeight : 0,
+			firstImageWidth  = firstImage ? firstImage.offsetWidth : 0,
+			firstImageSize   = gallery.classList.contains( 'vertical-style' ) ? firstImageHeight : firstImageWidth,
+			imageSize        = gallery.classList.contains( 'vertical-style' ) ? 60 : 80,
+			thumbItems       = firstImageSize ? parseInt( firstImageSize / imageSize ) : 5;
 
-		if ( ! buttons.length ) {
-			return;
-		}
-
-		buttons.forEach( function( el ) {
-			el.addEventListener( 'click', function() {
-				var nav = el.getAttribute( 'data-controls' );
-
-				if ( 'next' === nav ) {
-					thumbCarousel.goTo( 'next' );
-				} else {
-					thumbCarousel.goTo( 'prev' );
+		var thumbCarousel,
+			thumbOptions = {
+				loop: false,
+				container: '#product-thumbnail-images',
+				gutter: 10,
+				nav: false,
+				controls: true,
+				responsive: {
+					0: {
+						items: 5
+					},
+					720: {
+						items: ( thumbItems > 7 ? 7 : thumbItems )
+					},
+					992: {
+						items: thumbItems
+					}
 				}
-			} );
-		} );
-	}
-
-	// Reset carousel.
-	function resetCarousel() {
-		if ( imageCarousel && imageCarousel.goTo ) {
-			imageCarousel.goTo( 'first' );
 		}
 
-		if ( thumbCarousel && thumbCarousel.goTo ) {
-			thumbCarousel.goTo( 'first' );
-		}
-	}
-
-	// Update gallery.
-	function updateGallery( data, reset ) {
-		if ( ! data.length || document.documentElement.classList.contains( 'quick-view-open' ) ) {
-			return;
+		if (
+			window.matchMedia( '( min-width: 768px )' ).matches &&
+			gallery &&
+			gallery.classList.contains( 'vertical-style' )
+		) {
+			thumbOptions.axis = 'vertical';
 		}
 
-		// For Elementor Preview Mode.
-		if ( ! gallery ) {
-			gallery           = document.getElementsByClassName( 'product-gallery' )[0];
-			thumbOptions.axis = gallery.classList.contains( 'vertical-style' ) ? 'vertical' : 'horizontal';
+		if ( productThumbnails ) {
+			imageCarousel = tns( options );
+			thumbCarousel = tns( thumbOptions );
 		}
 
-		var images            = '',
-			thumbnails        = '',
-			variationId       = document.querySelector( 'form.variations_form [name=variation_id]' ),
-			defaultThumbnails = false;
-
-		for ( var i = 0, j = data.length; i < j; i++ ) {
-			if ( reset ) {
-				// For reset variation.
-				var size = data[i].full_src_w + 'x' + data[i].full_src_h;
-
-				images     += createImages( data[i].full_src, data[i].src, size );
-				thumbnails += createThumbnails( data[i].gallery_thumbnail_src );
-
-				if ( data[i].has_default_thumbnails ) {
-					defaultThumbnails = true;
-				}
-			} else if ( variationId && data[i][0].variation_id && parseInt( variationId.value ) === data[i][0].variation_id ) {
-				// Render new item for new Slider.
-				for ( var x = 1, y = data[i].length; x < y; x++ ) {
-					var size = data[i][x].full_src_w + 'x' + data[i][x].full_src_h;
-					images     += createImages( data[i][x].full_src, data[i][x].src, size );
-					thumbnails += createThumbnails( data[i][x].gallery_thumbnail_src );
-				}
-			}
-		}
-
-		// Destroy current slider.
-		( imageCarousel && imageCarousel.destroy ) ? imageCarousel.destroy() : false;
-		( thumbCarousel && thumbCarousel.destroy ) ? thumbCarousel.destroy() : false;
-
-		// If not have #product-thumbnail-images, create it.
-		if ( ! document.getElementById( 'product-thumbnail-images' ) ) {
-			var productThumbs = document.createElement( 'div' );
-
-			productThumbs.setAttribute( 'id', 'product-thumbnail-images' );
-			document.getElementsByClassName( 'product-thumbnail-images' )[0].appendChild( productThumbs );
-			document.getElementsByClassName( 'product-gallery' )[0].classList.add( 'has-product-thumbnails' );
-		}
-
-		// Append new markup html.
-		document.getElementById( 'product-images' ).innerHTML           = images;
-		document.getElementById( 'product-thumbnail-images' ).innerHTML = thumbnails;
-
-		// Rebuild new slider.
-		imageCarousel = ( imageCarousel && imageCarousel.rebuild ) ? imageCarousel.rebuild() : tns( options );
-		thumbCarousel = ( thumbCarousel && thumbCarousel.rebuild ) ? thumbCarousel.rebuild() : tns( thumbOptions );
-
-		if ( reset && ! defaultThumbnails ) {
-			( thumbCarousel && thumbCarousel.destroy ) ? thumbCarousel.destroy() : false;
-
-			// Remove all '#product-thumbnail-images' item.
-			document.querySelectorAll( '#product-thumbnail-images' ).forEach( function( el ) {
-				el.parentNode.removeChild( el );
-			} );
-		}
-
-		// Re-init easyzoom.
-		if ( 'function' === typeof( easyZoomHandle ) ) {
-			easyZoomHandle();
-		}
-
-		// Re-init Photo Swipe.
-		if ( 'function' === typeof( initPhotoSwipe ) ) {
-			initPhotoSwipe( '#product-images' );
-		}
-	}
-
-	// Carousel action.
-	function carouselAction() {
-		// Trigger variation.
-		jQuery( 'form.variations_form' ).on( 'found_variation', function() {
-			resetCarousel();
-
-			if ( 'undefined' !== typeof( woostify_variation_gallery ) && woostify_variation_gallery.length ) {
-				updateGallery( woostify_variation_gallery );
-			}
-		});
-
-		// Trigger reset.
-		jQuery( '.reset_variations' ).on( 'click', function(){
-			// Apply for slider layout.
-			if ( ! document.body.classList.contains( 'has-gallery-slider-layout' ) ) {
+		// Arrow event.
+		function arrowsEvent() {
+			if ( ! thumbCarousel ) {
 				return;
 			}
 
-			resetCarousel();
+			var buttons = document.querySelectorAll( '.product-images .tns-controls button' );
 
-			if ( 'undefined' !== typeof( woostify_variation_gallery ) && woostify_variation_gallery.length ) {
-				updateGallery( woostify_default_gallery, true );
+			if ( ! buttons.length ) {
+				return;
 			}
 
-			if ( document.body.classList.contains( 'elementor-editor-active' ) || document.body.classList.contains( 'elementor-editor-preview' ) ) {
-				if ( ! document.getElementById( 'product-thumbnail-images' ) ) {
-					document.querySelector( '.product-gallery' ).classList.remove( 'has-product-thumbnails' );
+			buttons.forEach(
+				function( el ) {
+					el.addEventListener(
+						'click',
+						function() {
+							var nav = el.getAttribute( 'data-controls' );
+
+							if ( 'next' === nav ) {
+								thumbCarousel.goTo( 'next' );
+							} else {
+								thumbCarousel.goTo( 'prev' );
+							}
+						}
+					);
 				}
-			} else if ( ! productThumbnails ) {
-				gallery.classList.remove( 'has-product-thumbnails' );
+			);
+		}
+
+		// Reset carousel.
+		function resetCarousel() {
+			if ( imageCarousel && imageCarousel.goTo ) {
+				imageCarousel.goTo( 'first' );
 			}
-		});
-	}
-	carouselAction();
 
-	// Grid and One column to caousel layout on mobile.
-	woostifyGalleryCarouselMobile();
+			if ( thumbCarousel && thumbCarousel.goTo ) {
+				thumbCarousel.goTo( 'first' );
+			}
+		}
 
-	// Load event.
-	window.addEventListener( 'load', function() {
-		woostifyStickySummary();
-		arrowsEvent();
-	} );
+		// Update gallery.
+		function updateGallery( data, reset ) {
+			if ( ! data.length || document.documentElement.classList.contains( 'quick-view-open' ) ) {
+				return;
+			}
 
-	// For Elementor Preview Mode.
-	if ( 'function' === typeof( onElementorLoaded ) ) {
-		onElementorLoaded( function() {
-			window.elementorFrontend.hooks.addAction( 'frontend/element_ready/global', function() {
-				if ( document.getElementById( 'product-thumbnail-images' ) ) {
-					renderSlider( '#product-images', options );
+			// For Elementor Preview Mode.
+			if ( ! gallery ) {
+				gallery           = document.getElementsByClassName( 'product-gallery' )[0];
+				thumbOptions.axis = gallery.classList.contains( 'vertical-style' ) ? 'vertical' : 'horizontal';
+			}
 
-					thumbOptions.axis = document.getElementsByClassName( 'product-gallery' )[0].classList.contains( 'vertical-style' ) ? 'vertical' : 'horizontal';
-					renderSlider( '#product-thumbnail-images', thumbOptions );
+			var images            = '',
+				thumbnails        = '',
+				variationId       = document.querySelector( 'form.variations_form [name=variation_id]' ),
+				defaultThumbnails = false;
+
+			for ( var i = 0, j = data.length; i < j; i++ ) {
+				if ( reset ) {
+					// For reset variation.
+					var size = data[i].full_src_w + 'x' + data[i].full_src_h;
+
+					images     += createImages( data[i].full_src, data[i].src, size );
+					thumbnails += createThumbnails( data[i].gallery_thumbnail_src );
+
+					if ( data[i].has_default_thumbnails ) {
+						defaultThumbnails = true;
+					}
+				} else if ( variationId && data[i][0].variation_id && parseInt( variationId.value ) === data[i][0].variation_id ) {
+					// Render new item for new Slider.
+					for ( var x = 1, y = data[i].length; x < y; x++ ) {
+						var size        = data[i][x].full_src_w + 'x' + data[i][x].full_src_h;
+							images     += createImages( data[i][x].full_src, data[i][x].src, size );
+							thumbnails += createThumbnails( data[i][x].gallery_thumbnail_src );
+					}
 				}
-				carouselAction();
+			}
+
+			// Destroy current slider.
+			( imageCarousel && imageCarousel.destroy ) ? imageCarousel.destroy() : false;
+			( thumbCarousel && thumbCarousel.destroy ) ? thumbCarousel.destroy() : false;
+
+			// If not have #product-thumbnail-images, create it.
+			if ( ! document.getElementById( 'product-thumbnail-images' ) ) {
+				var productThumbs = document.createElement( 'div' );
+
+				productThumbs.setAttribute( 'id', 'product-thumbnail-images' );
+				document.getElementsByClassName( 'product-thumbnail-images' )[0].appendChild( productThumbs );
+				document.getElementsByClassName( 'product-gallery' )[0].classList.add( 'has-product-thumbnails' );
+			}
+
+			// Append new markup html.
+			document.getElementById( 'product-images' ).innerHTML           = images;
+			document.getElementById( 'product-thumbnail-images' ).innerHTML = thumbnails;
+
+			// Rebuild new slider.
+			imageCarousel = ( imageCarousel && imageCarousel.rebuild ) ? imageCarousel.rebuild() : tns( options );
+			thumbCarousel = ( thumbCarousel && thumbCarousel.rebuild ) ? thumbCarousel.rebuild() : tns( thumbOptions );
+
+			if ( reset && ! defaultThumbnails ) {
+				( thumbCarousel && thumbCarousel.destroy ) ? thumbCarousel.destroy() : false;
+
+				// Remove all '#product-thumbnail-images' item.
+				document.querySelectorAll( '#product-thumbnail-images' ).forEach(
+					function( el ) {
+						el.parentNode.removeChild( el );
+					}
+				);
+			}
+
+			// Re-init easyzoom.
+			if ( 'function' === typeof( easyZoomHandle ) ) {
+				easyZoomHandle();
+			}
+
+			// Re-init Photo Swipe.
+			if ( 'function' === typeof( initPhotoSwipe ) ) {
+				initPhotoSwipe( '#product-images' );
+			}
+		}
+
+		// Carousel action.
+		function carouselAction() {
+			// Trigger variation.
+			jQuery( 'form.variations_form' ).on(
+				'found_variation',
+				function() {
+					resetCarousel();
+
+					if ( 'undefined' !== typeof( woostify_variation_gallery ) && woostify_variation_gallery.length ) {
+						updateGallery( woostify_variation_gallery );
+					}
+				}
+			);
+
+			// Trigger reset.
+			jQuery( '.reset_variations' ).on(
+				'click',
+				function(){
+					// Apply for slider layout.
+					if ( ! document.body.classList.contains( 'has-gallery-slider-layout' ) ) {
+						return;
+					}
+
+					resetCarousel();
+
+					if ( 'undefined' !== typeof( woostify_variation_gallery ) && woostify_variation_gallery.length ) {
+						updateGallery( woostify_default_gallery, true );
+					}
+
+					if ( document.body.classList.contains( 'elementor-editor-active' ) || document.body.classList.contains( 'elementor-editor-preview' ) ) {
+						if ( ! document.getElementById( 'product-thumbnail-images' ) ) {
+							document.querySelector( '.product-gallery' ).classList.remove( 'has-product-thumbnails' );
+						}
+					} else if ( ! productThumbnails ) {
+						gallery.classList.remove( 'has-product-thumbnails' );
+					}
+				}
+			);
+		}
+		carouselAction();
+
+		// Grid and One column to caousel layout on mobile.
+		woostifyGalleryCarouselMobile();
+
+		// Load event.
+		window.addEventListener(
+			'load',
+			function() {
+				woostifyStickySummary();
 				arrowsEvent();
-			} );
-		} );
+			}
+		);
+
+		// For Elementor Preview Mode.
+		if ( 'function' === typeof( onElementorLoaded ) ) {
+			onElementorLoaded(
+				function() {
+					window.elementorFrontend.hooks.addAction(
+						'frontend/element_ready/global',
+						function() {
+							if ( document.getElementById( 'product-thumbnail-images' ) ) {
+								renderSlider( '#product-images', options );
+
+								thumbOptions.axis = document.getElementsByClassName( 'product-gallery' )[0].classList.contains( 'vertical-style' ) ? 'vertical' : 'horizontal';
+								renderSlider( '#product-thumbnail-images', thumbOptions );
+							}
+							carouselAction();
+							arrowsEvent();
+						}
+					);
+				}
+			);
+		}
 	}
-} );
+);
