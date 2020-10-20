c222c395b22d141fbce27a6d30c29ded40246fd9
duongancol <mr.duongancol@gmail.com>
2020-10-20 11:06:08 +0700

    Fix WP API error in product single

--- inc/class-woostify.php
+++ inc/class-woostify.php
@@ -546,8 +546,9 @@ if ( ! class_exists( 'Woostify' ) ) {
 				true
 			);
 
-			if ( class_exists( 'woocommerce' ) ) {
+			if ( class_exists( 'woocommerce' ) && is_checkout() ) {
 				$wc_total = WC()->cart->get_totals();
+				var_dump($wc_total);
 				$price    = (float) $wc_total['subtotal'] - (float) $wc_total['discount_total'];
 
 				wp_localize_script(
