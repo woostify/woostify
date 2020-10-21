c378fb9daf8974fd45fb35c5faed25a709b89a6d
pham viet thang <phamvietthangcntt9x@gmail.com>
2020-10-21 16:37:28 +0700

    icon menu mobie dropdown effect

--- inc/customizer/class-woostify-get-css.php
+++ inc/customizer/class-woostify-get-css.php
@@ -125,10 +125,6 @@ class Woostify_Get_CSS {
 				.has-header-layout-1 .site-navigation {
 					text-align: left;
 				}
-				.sidebar-menu .arrow-icon.active {
-				    -webkit-transform: rotate(0deg) !important;
-				    transform: rotate(0deg) !important;
-				}
 				.has-header-layout-3 .header-layout-3 .wrap-toggle-sidebar-menu {
 					display: block !important;
 				}
