ff08f5a0bace8bced654ac9d4c6714b1e4ae35c3
duongancol <mr.duongancol@gmail.com>
2020-12-05 19:06:21 +0700

    release pro 1.4.5

--- functions.php
+++ functions.php
@@ -7,7 +7,7 @@
 
 // Define constants.
 define( 'WOOSTIFY_VERSION', '1.7.8' );
-define( 'WOOSTIFY_PRO_MIN_VERSION', '1.4.4' );
+define( 'WOOSTIFY_PRO_MIN_VERSION', '1.4.5' );
 define( 'WOOSTIFY_THEME_DIR', get_template_directory() . '/' );
 define( 'WOOSTIFY_THEME_URI', get_template_directory_uri() . '/' );
 
