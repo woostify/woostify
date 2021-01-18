b821aa5ebcc7e54f2ede93a5cccd18d07dbd53c0
ttn <thaobtf@outlook.com>
2021-01-18 15:59:30 +0700

    add condition for get metabox function

--- inc/woostify-functions.php
+++ inc/woostify-functions.php
@@ -579,6 +579,10 @@ if ( ! function_exists( 'woostify_get_metabox' ) ) {
 	 * @param string $metabox_name Metabox option name.
 	 */
 	function woostify_get_metabox( $page_id = false, $metabox_name ) {
+		if ( is_archive() ) {
+			return 'default';
+		}
+
 		$page_id             = $page_id ? intval( $page_id ) : woostify_get_page_id();
 		$metabox             = get_post_meta( $page_id, $metabox_name, true );
 		$is_product_category = class_exists( 'woocommerce' ) && is_product_category();
