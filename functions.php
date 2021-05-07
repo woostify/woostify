664116e1ba9e7779cbe52ceebd606017ef3c8dee
Dylan Ngo <mr.duongancol@gmail.com>
2021-05-07 14:52:40 +0700

    release theme version 1.8.7, pro version 1.5.3

--- functions.php
+++ functions.php
@@ -6,8 +6,8 @@
  */
 
 // Define constants.
-define( 'WOOSTIFY_VERSION', '1.8.6' );
-define( 'WOOSTIFY_PRO_MIN_VERSION', '1.5.2' );
+define( 'WOOSTIFY_VERSION', '1.8.7' );
+define( 'WOOSTIFY_PRO_MIN_VERSION', '1.5.3' );
 define( 'WOOSTIFY_THEME_DIR', get_template_directory() . '/' );
 define( 'WOOSTIFY_THEME_URI', get_template_directory_uri() . '/' );
 
