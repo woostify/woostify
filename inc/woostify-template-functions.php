a6f05fdfc46b0af5476f5623e29d831893a726ca
pham viet thang <phamvietthangcntt9x@gmail.com>
2021-06-14 16:32:34 +0700

    update image product categories on page header

--- inc/woostify-template-functions.php
+++ inc/woostify-template-functions.php
@@ -878,9 +878,22 @@ if ( ! function_exists( 'woostify_page_header' ) ) {
 		if ( ! $page_header || $disable_page_header ) {
 			return;
 		}
+		if ( class_exists( 'WooCommerce' ) && is_product_category() ) {
+			global $wp_query;
+			$cat          = $wp_query->get_queried_object();
+			$thumbnail_id = get_term_meta( $cat->term_id, 'thumbnail_id', true );
+			$image        = wp_get_attachment_url( $thumbnail_id );
+
+			if ( $image ) {
+				$images = 'style="background-image: url(' . $image . ')"';
+			} else {
+				$images = '';
+			}
+		}
+
 		?>
 
-		<div class="page-header">
+		<div class="page-header" <?php echo $images; // phpcs:ignore ?> >
 			<div class="<?php echo esc_attr( $classes ); ?>">
 				<?php do_action( 'woostify_page_header_start' ); ?>
 
