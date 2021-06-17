ea55a0708a600d03fade8e427bc903b1fb569bf3
tranthaongc <thaobtf@outlook.com>
2021-06-17 17:21:12 +0700

    escape svg icon

--- inc/woostify-template-functions.php
+++ inc/woostify-template-functions.php
@@ -2445,7 +2445,7 @@ if ( ! function_exists( 'woostify_sticky_footer_bar' ) ) {
 							<?php if ( '' !== $item->icon ) { ?>
 								<span class="woostify-item-list-item__icon ">
 							<span class="woositfy-sfb-icon">
-								<?php echo woostify_fetch_svg_icon( $item->icon ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
+								<?php echo wp_kses( woostify_fetch_svg_icon( $item->icon ), woostify_allow_tags_svg() ); ?>
 							</span>
 							<span class="theme-item-count shop-cart-count <?php echo $options['header_shop_hide_zero_value_cart_count'] ? 'hide-zero-val' : ''; ?>"><?php echo esc_html( $count ); ?></span>
 						</span>
@@ -2462,7 +2462,7 @@ if ( ! function_exists( 'woostify_sticky_footer_bar' ) ) {
 							<?php if ( '' !== $item->icon ) { ?>
 								<span class="woostify-item-list-item__icon">
 								<span class="woositfy-sfb-icon header-search-icon">
-									<?php echo woostify_fetch_svg_icon( $item->icon ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
+									<?php echo wp_kses( woostify_fetch_svg_icon( $item->icon ), woostify_allow_tags_svg() ); ?>
 								</span>
 							</span>
 							<?php } ?>
@@ -2478,7 +2478,7 @@ if ( ! function_exists( 'woostify_sticky_footer_bar' ) ) {
 							<?php if ( '' !== $item->icon ) { ?>
 								<span class="woostify-item-list-item__icon">
 							<span class="woositfy-sfb-icon">
-								<?php echo woostify_fetch_svg_icon( $item->icon ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
+								<?php echo wp_kses( woostify_fetch_svg_icon( $item->icon ), woostify_allow_tags_svg() ); ?>
 							</span>
 						</span>
 							<?php } ?>
