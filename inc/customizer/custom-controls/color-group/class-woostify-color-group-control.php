eeb46e5802fb8cee1e6fa0f60e9f8395c8f42314
pdutie94 <ptiencntt94@gmail.com>
2021-07-06 09:06:33 +0700

    Color group reset to previous saved value instead of default value

--- inc/customizer/custom-controls/color-group/class-woostify-color-group-control.php
+++ inc/customizer/custom-controls/color-group/class-woostify-color-group-control.php
@@ -190,7 +190,7 @@ class Woostify_Color_Group_Control extends WP_Customize_Control {
 				$btn_id      = isset( $btn_id_arr[1] ) ? explode( ']', $btn_id_arr[1] )[0] : $btn_id_arr[0];
 				$reset_value = $setting->default;
 				?>
-				<input type="hidden" class="color-group-value color-group-value-<?php echo esc_attr( $btn_id ); ?>" data-reset_value="<?php echo esc_attr( $reset_value ); ?>" <?php echo $link; //phpcs:ignore?> value="<?php echo esc_attr( $this->value( $k ) ); ?>">
+				<input type="hidden" class="color-group-value color-group-value-<?php echo esc_attr( $btn_id ); ?>" data-reset_value="<?php echo esc_attr( $this->value( $k ) ); ?>" <?php echo $link; //phpcs:ignore?> value="<?php echo esc_attr( $this->value( $k ) ); ?>">
 			<?php } ?>
 		</div>
 		<?php
