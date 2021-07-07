5a6f28e6805daf7c165da740d602265267d22674
Dylan Ngo <mr.duongancol@gmail.com>
2021-07-07 21:45:19 +0700

    release theme version 1.9.3

--- functions.php
+++ functions.php
@@ -6,7 +6,7 @@
  */
 
 // Define constants.
-define( 'WOOSTIFY_VERSION', '1.9.2' );
+define( 'WOOSTIFY_VERSION', '1.9.3' );
 define( 'WOOSTIFY_PRO_MIN_VERSION', '1.5.6' );
 define( 'WOOSTIFY_THEME_DIR', get_template_directory() . '/' );
 define( 'WOOSTIFY_THEME_URI', get_template_directory_uri() . '/' );
