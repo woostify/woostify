60cc4fda70f724ff5d364f18be6b8dccc8b91dec
Dylan Ngo <mr.duongancol@gmail.com>
2021-08-03 15:13:39 +0700

    release theme version 1.9.6

--- Gulpfile.js
+++ Gulpfile.js
@@ -8,7 +8,7 @@
 
 let theme       = 'woostify',
 	site_name   = 'dev',
-	theme_ver   = '1.9.5',
+	theme_ver   = '1.9.6',
 	gulp        = require( 'gulp' ),
 	zip         = require( 'gulp-zip' ),
 	autoLoad    = require( 'gulp-load-plugins' )(),
