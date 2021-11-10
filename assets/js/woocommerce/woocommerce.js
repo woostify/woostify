cf09421756b889738a3c7fb77b2be047dc7fb14e
_ttn <tranthaongc@gmail.com>
2021-11-10 14:17:40 +0700

    phpcs space

--- assets/js/woocommerce/woocommerce.js
+++ assets/js/woocommerce/woocommerce.js
@@ -146,7 +146,7 @@ function woostifyInfiniteScroll( addEventClick ) {
 			}
 
 			if ( '1' === woostify_woocommerce_general.is_active_wvs ) {
-				jQuery('.variations_form').each(
+				jQuery( '.variations_form' ).each(
 					function(){
 						jQuery( this ).wc_variation_form();
 					}
