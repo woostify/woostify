dc03bbfb1c79f7045f0402180b568e973e52ae71
Dylan Ngo <mr.duongancol@gmail.com>
2021-11-22 23:11:44 +0700

    release new 2.0

--- Gulpfile.js
+++ Gulpfile.js
@@ -8,7 +8,7 @@
 
 let theme       = 'woostify',
 	site_name   = 'dev',
-	theme_ver   = '1.9.9',
+	theme_ver   = '2.0.0',
 	gulp        = require( 'gulp' ),
 	zip         = require( 'gulp-zip' ),
 	autoLoad    = require( 'gulp-load-plugins' )(),
