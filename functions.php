306c6eed98c8d00dfe8843143cbf1ea8e8a33ec2
DESKTOP-CDO0SQ2\Admin <toanbuitan@gmail.com>
2023-06-23 22:54:01 +0700

    feature-custom-checkout-login-wc update checkout login woostify

--- functions.php
+++ functions.php
@@ -50,3 +50,31 @@ require_once WOOSTIFY_THEME_DIR . 'inc/compatibility/class-woostify-divi-builder
 /**
  * Note: Do not add any custom code here. Please use a custom plugin so that your customizations aren't lost during updates.
  */
+
+add_action( 'woocommerce_before_checkout_form', 'woostify_woocommerce_before_checkout_form_action' );
+function woostify_woocommerce_before_checkout_form_action( $checkout ){
+
+	$items = WC()->cart->get_cart();
+
+	$product_cats_check = array(20,22);
+
+	if ($items) {
+		foreach ($items as $key => $cart_item ) {
+			$product_id = $cart_item['product_id'];
+			$terms = get_the_terms ( $product_id, 'product_cat' );
+			$cart_product_cat_ids = array();
+			foreach ( $terms as $term ) {
+			  $cat_id = $term->term_id;
+				array_push($cart_product_cat_ids, $cat_id);
+			}
+			if ( count(array_intersect($product_cats_check, $cart_product_cat_ids)) !== 0 ) {
+				$wc_myaccount_url = get_permalink( get_option( 'woocommerce_myaccount_page_id' ) );
+				if ( is_checkout() && ! is_user_logged_in() ) {
+					wp_safe_redirect( $wc_myaccount_url );
+					exit();
+				}
+			}
+		}
+	}
+
+}
