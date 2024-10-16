6cc8c665a44afe1e0df38aa490049aaa941f594a
ngothaiduong2311@gmail.com <>
2024-10-16 08:49:58 +0700

    release woostify theme 2.3.7

--- Gulpfile.js
+++ Gulpfile.js
@@ -8,7 +8,7 @@
 
 let theme       = 'woostify',
 	site_name   = 'dev',
-	theme_ver   = '2.3.6',
+	theme_ver   = '2.3.7',
 	gulp        = require( 'gulp' ),
 	zip         = require( 'gulp-zip' ),
 	autoLoad    = require( 'gulp-load-plugins' )(),
